c0cc2585c81dc953878c847bfc283ba5adf7fc70
KurokuTetsuya <perumnas.good@gmail.com>
2020-12-15 19:51:43 +0700

    fix(ci): Solve eslint

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -3,10 +3,11 @@ import * as config from "../config";
 
 export class Client {
     public readonly config = config;
-    public readonly logger = createLogger()
+    public readonly logger = createLogger();
+    // eslint-disable-next-line
     constructor() {}
 
-    public build() {
+    public build(): void {
         this.logger.warn("Not implemented!");
     }
 }
