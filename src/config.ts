8678a4b46b6581bc62ca5f9dbb189449f3c17173
KurokuTetsuya <perumnas.good@gmail.com>
2020-12-15 20:09:55 +0700

    feat(Server): Add server port

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@ export const token = process.env.TOKEN;
 export const isDev = process.env.NODE_ENV === "development";
 export const appID = process.env.APP_ID;
 export const publicKey = process.env.PUBLIC_KEY;
+export const serverPort = 3000;
