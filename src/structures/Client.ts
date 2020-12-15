1b833dfc0561ed1fffea1c69177ff5434c6e477a
Zen <45705890+KurokuTetsuya@users.noreply.github.com>
2020-12-15 21:05:22 +0700

    feat(express): Create express server (#11)
    * feat(Server): Add server port
    
    * feat(Server): Add interaction types
    
    * feat(Server): create Server
    
    * fix(Server): Add body-parser to receive raw body
    
    * chore(deps): Move @types/express to dev dependencies
    
    * feat(Server): Add / path

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,13 +1,15 @@
 import { createLogger } from "../utils/Logger";
 import * as config from "../config";
+import { ExpressServer } from "./Server";
 
 export class Client {
     public readonly config = config;
     public readonly logger = createLogger();
+    public readonly express = new ExpressServer(this);
     // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-useless-constructor
     public constructor() {}
 
     public build(): void {
-        this.logger.warn("Not implemented!");
+        this.express.start();
     }
 }
