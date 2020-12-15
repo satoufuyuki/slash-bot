881919727cb8471259ae59b3c8e5e4b04a65ae87
Zen <45705890+KurokuTetsuya@users.noreply.github.com>
2020-12-15 19:55:53 +0700

    refactor(client): Disable specific rules
    Co-authored-by: hzmi <contact@hzmi.xyz>

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -4,8 +4,8 @@ import * as config from "../config";
 export class Client {
     public readonly config = config;
     public readonly logger = createLogger();
-    // eslint-disable-next-line
-    constructor() {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-useless-constructor
+    public constructor() {}
 
     public build(): void {
         this.logger.warn("Not implemented!");
